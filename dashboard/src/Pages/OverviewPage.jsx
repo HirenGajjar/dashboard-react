3ab02d460d702d80e43317894c302438c24cfa5f
HirenGajjar <gajjarhiren111@gmail.com>
2024-10-03 16:00:43 -0700

    Add Header component

--- dashboard/src/Pages/OverviewPage.jsx
+++ dashboard/src/Pages/OverviewPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "../Components/Common/Header";
 
 function OverviewPage() {
-  return <div>OverviewPage</div>;
+  return (
+    <>
+      <div className="flex-1 overflow-auto relative z-10">
+        <Header title="Overview" />
+      </div>
+    </>
+  );
 }
 
 export default OverviewPage;
--- dashboard/src/Pages/ProductsPage.jsx
+++ dashboard/src/Pages/ProductsPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "../Components/Common/Header";
 
 function ProductsPage() {
-  return <div>ProductsPage</div>;
+  return (
+    <>
+      <div className="flex-1 overflow-auto relative z-10">
+        <Header title="Products" />
+      </div>
+    </>
+  );
 }
 
 export default ProductsPage;
