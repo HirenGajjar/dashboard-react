fb0f69f17982310aae108a5017d6b559c4f2443a
HirenGajjar <gajjarhiren111@gmail.com>
2024-10-03 15:16:27 -0700

    Add route

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
+import { OverviewPage } from "./Pages/OverviewPage";
+import { ProductsPage } from "./Pages/ProductsPage";
 function App() {
   return (
     <>
-      <div className=""></div>
+      <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+        <Routes>
+          <Route path="/" element={<OverviewPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
